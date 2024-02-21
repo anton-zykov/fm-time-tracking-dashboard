9b0563a8b92fdee2a56770744d0ef3ebddd1f516
Zykov Anton <anton.zykov1@gmail.com>
2024-02-21 22:44:42 +0100

    Add script for creating cards based on fetched data

---
+++ index.js
@@ -0,0 +1,57 @@
+const timeframeAdjectives = {
+  'Day': 'Daily',
+  'Week': 'Weekly',
+  'Month': 'Monthly',
+};
+
+let currentTimeframe = 'Month';
+
+class Card {
+  constructor ({ title, timeframes }) {
+    this.title = title;
+    this.timeframes = timeframes;
+    
+    this.element = this.createElement();
+    this.subElements = {
+      hours: this.element.querySelector('.card__hours'),
+      previous: this.element.querySelector('.card__previous'),
+    };
+    this.updateHours();
+  }
+
+  createTemplate () {
+    return `
+      <div class="card">
+        <img class="card__icon" src="./images/icon-${this.title.toLowerCase().replace(' ', '-')}.svg" alt="">
+        <div class="card__content">
+          <p class="card__title">${this.title}</p>
+          <a class="card__more" href="/" aria-label="More on ${this.title}">
+            <img src="./images/icon-ellipsis.svg" alt="">
+          </a>
+          <span class="card__hours"></span>
+          <span class="card__previous"></span>
+        </div>
+      </div>
+    `;
+  }
+
+  createElement () {
+    const container = document.createElement('div');
+    container.innerHTML = this.createTemplate();
+    return container.firstElementChild;
+  }
+
+  updateHours () {
+    this.subElements.hours.textContent = this.timeframes[timeframeAdjectives[currentTimeframe].toLowerCase()].current + 'hrs';
+    this.subElements.previous.textContent = 
+      'Last ' + currentTimeframe + ' - ' + this.timeframes[timeframeAdjectives[currentTimeframe].toLowerCase()].previous + 'hrs';
+  }
+}
+
+let cards = [];
+fetch('./data.json')
+  .then((response) => (response.json()))
+  .then((data) => {
+    cards = data.map((category) => (new Card(category)));
+    document.body.append(...cards.map((card) => card.element));
+  });
